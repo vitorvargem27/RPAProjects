0ecf31237804b77319fb2b147b0f76d0578b88f3
vitorVargem <tp.vitor.vargem@gmail.com>
2023-12-14 15:40:17 -0300

    Change the mouse actions to general actions

--- googleSearch.py
+++ googleSearch.py
@@ -1,19 +1,19 @@
 import pyautogui as mouse
 import pyautogui as timeLoad
+import pyautogui as action
 
 #abrindo o windows
 timeLoad.sleep(2)
-mouse.moveTo(4184, 1602)
-mouse.click(4184, 1602)
+action.press('win')
 
 #pesquisando google no windows
 timeLoad.sleep(1)
-mouse.typewrite('google')
+action.typewrite('google')
 timeLoad.sleep(1)
-mouse.press('enter')
+action.press('enter')
 
 #pesquisando facebook no google
-timeLoad.sleep(4)
+timeLoad.sleep(2)
 mouse.moveTo(3330, 98)
 mouse.click(3330,98)
 mouse.typewrite('facebook.com')
@@ -21,4 +21,3 @@ mouse.typewrite('facebook.com')
 #pressionando enter para acessar o facebook
 timeLoad.sleep(1)
 mouse.press("enter")
-
--- openNewGoogleGuide.py
+++ openNewGoogleGuide.py
@@ -4,14 +4,13 @@ import pyautogui as action
 
 #abrindo a aba de pesquisa do Windows
 timeLoad.sleep(1.5)
-mouse.moveTo(4184, 1602)
-mouse.click(4184, 1602)
+action.press('win')
 
 #abrindo o google chrome automaticamente
 timeLoad.sleep(1)
-mouse.typewrite('google')
+action.typewrite('google')
 timeLoad.sleep(1)
-mouse.press("enter")
+action.press("enter")
 
 #abrindo outra pasta dentro do google
 timeLoad.sleep(3)
