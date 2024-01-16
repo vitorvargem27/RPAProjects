86519a8875fd378cb27f73460ad0505c6e3537c4
vitorVargem <tp.vitor.vargem@gmail.com>
2024-01-16 16:20:14 -0300

    Forms Raply Updated

--- automatedForms/formsAutomatedReply.py
+++ automatedForms/formsAutomatedReply.py
@@ -3,12 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
 import pyautogui as timeLoad
+import pyautogui as mouse
 
 #perguntanoo se a pessoa tem filhos
 haveKids = str(input('Are you have a kids?[\033[1:31mN\033[m]/[\033[1:32mY\033[m]\n')).strip().upper()
 
 #perguntando a cor favorita da pessoa
-favoriteColor = int(input("What's are you favorite color?\n"
+favoriteColor = int(input("What's your favorite color?\n"
                           "[1] BLUE\n"
                           "[2] YELLOW\n"
                           "[3] RED\n"
@@ -17,7 +18,7 @@ favoriteColor = int(input("What's are you favorite color?\n"
                           "[6] GREEN\n\n").strip())
 
 while favoriteColor < 1 and favoriteColor > 6 :
-    favoriteColor = int(input("What's are you favorite color?\n"
+    favoriteColor = int(input("What's your favorite color?\n"
                               "[1] BLUE\n"
                               "[2] YELLOW\n"
                               "[3] RED\n"
@@ -25,7 +26,9 @@ while favoriteColor < 1 and favoriteColor > 6 :
                               "[5] BLACK\n"
                               "[6] GREEN\n").strip())
 
-formsAvaliate = int(input("What's the note that you give to this form? (Between 1 and 5)\n").strip())
+formsValidateStars = int(input("What's the note that you give in stars to this form? (Between 1 to 5)\n").strip())
+formsValidateService = int(input("What's the note that you give in degree to this form? (Between 1 to 4)\n").strip())
+formsValidateDifficulty = int(input("What's the note that you give in degree to this form? (Between 1 to 4)\n").strip())
 
 navigate = seleniumOptions.Chrome()
 navigate.get('https://www.jotform.com/221436066464051')
@@ -38,6 +41,9 @@ timeLoad.sleep(1)
 navigate.find_element(By.NAME, 'q4_email').send_keys('teste@gmail.com')
 timeLoad.sleep(2)
 
+mouse.scroll(-400)
+timeLoad.sleep(1)
+
 #puxando a caixa de opções
 optionsForSelect = navigate.find_element(By.ID, 'input_5')
 selectedItem = Select(optionsForSelect)
@@ -48,12 +54,16 @@ timeLoad.sleep(2)
 
 #respondendo a caixa de filhos do formulário à partir da resposta do usuário
 if haveKids == 'N' :
-    timeLoad.sleep(1.5)
+    timeLoad.sleep(2)
     navigate.find_element(By.ID, 'label_input_6_1').click()
 
 else:
-    timeLoad.sleep(1.5)
+    timeLoad.sleep(2)
     navigate.find_element(By.ID, 'label_input_6_0').click()
+
+timeLoad.sleep(2)
+mouse.scroll(-400)
+timeLoad.sleep(1)
 
 #respondendo a caixa de cores do formulário referente a cor escolhida
 if favoriteColor == 1 :
@@ -80,33 +90,69 @@ elif favoriteColor == 6 :
     timeLoad.sleep(1)
     navigate.find_element(By.ID, 'label_input_7_5').click()
 
-timeLoad.sleep(4)
+timeLoad.sleep(2)
+mouse.scroll(-400)
+timeLoad.sleep(1)
 
 #marcando a nota em estrelas à partir da resposta do usuário
-if formsAvaliate == 1 :
+if formsValidateStars == 1 :
     timeLoad.sleep(1)
     navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[7]/div/div/div[1]').click()
 
-elif formsAvaliate == 2 :
+elif formsValidateStars == 2 :
     timeLoad.sleep(1)
     navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[7]/div/div/div[2]').click()
 
-elif formsAvaliate == 3 :
+elif formsValidateStars == 3 :
     timeLoad.sleep(1)
     navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[7]/div/div/div[3]').click()
 
-elif formsAvaliate == 4 :
+elif formsValidateStars == 4 :
     timeLoad.sleep(1)
     navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[7]/div/div/div[4]').click()
 
-elif formsAvaliate == 5 :
+elif formsValidateStars == 5 :
     timeLoad.sleep(1)
     navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[7]/div/div/div[5]').click()
 
 timeLoad.sleep(2)
-navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[2]/td[4]/input').click()
+
+#avaliação do serviço do formulário à partir da resposta do usuário
+if formsValidateService == 1 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[2]/td[1]/input').click()
+
+elif formsValidateService == 2 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[2]/td[2]/input').click()
+
+elif formsValidateService == 3 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[2]/td[3]/input').click()
+
+elif formsValidateService == 4 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[2]/td[4]/input').click()
+
 timeLoad.sleep(2)
-navigate.find_element(By.XPATH,'/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[3]/td[4]/input').click()
+
+#validação de avaliação de dificuldade à partir da resposta do usuário
+if formsValidateDifficulty == 1 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH,'/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[3]/td[1]/input').click()
+
+elif formsValidateDifficulty == 2 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH,'/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[3]/td[2]/input').click()
+
+elif formsValidateDifficulty == 3 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH,'/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[3]/td[3]/input').click()
+
+elif formsValidateDifficulty == 4 :
+    timeLoad.sleep(1)
+    navigate.find_element(By.XPATH,'/html/body/form/div[1]/ul/li[8]/div/table/tbody/tr[3]/td[4]/input').click()
+
 timeLoad.sleep(2)
 navigate.find_element(By.XPATH, '/html/body/form/div[1]/ul/li[9]/div/div/button').click()
 
